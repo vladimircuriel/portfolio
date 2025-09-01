a1022ad7a7d81482804b0717c0a93b7592a401a6
NightmareVCO <vladimircuriel@outlook.com>
2025-09-01 13:25:00 -0400

    refactor: standardize code formatting and improve GitHub Actions workflow for Node.js 22

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -1,9 +1,9 @@
 import type { Favicon } from "@/types/config.ts";
 
 export const defaultFavicons: Favicon[] = [
-  {
-    src: '/favicon/favicon.svg',
-    theme: 'light',
-    sizes: '32x32',
-  },
+	{
+		src: "/favicon/favicon.svg",
+		theme: "light",
+		sizes: "32x32",
+	},
 ];
